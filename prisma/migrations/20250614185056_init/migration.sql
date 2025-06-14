1ca33ded62888a582ce5649cce67054377754e6b
Noor Ul Ain <noornasim908@gmail.com>
2025-06-14 23:55:32 +0500

    Updated schema to have boards table

---
+++ prisma/migrations/20250614185056_init/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Board" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Board_pkey" PRIMARY KEY ("id")
+);
